6ab71ac81c579b3aebd738b547d85958b6ca89e4
ChrisH81 <chrishurst1981@icloud.com>
2017-05-24 15:17:47 +0200

    Add Model Specs: Associations

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,11 +1,15 @@
 class Event < ApplicationRecord
   belongs_to :user
-
-  validates :description, presence: true, length: { maximum: 500 }
-  validates :name, presence: true, length: { maximum: 50 }
-  validates :location, presence: true
-  validates :starts_at, presence: true
-  validates :ends_at, presence: true
-
   has_and_belongs_to_many :categories
-end
+
+   validates :name, presence: true
+   validates :description, presence: true, length: { maximum: 500 }
+
+  def bargain?
+    price < 30
+  end
+
+  def self.order_by_price
+    order(:price)
+  end
+ end
